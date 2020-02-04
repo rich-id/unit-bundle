652f06dda2da60e129bc481ec0ca99cd89269034
Nicolas Guilloux <nguilloux@richcongress.com>
2020-02-04 10:49:55 +0100

    Fix declare_types

--- Tests/TestCase/ControllerTestCaseTest.php
+++ Tests/TestCase/ControllerTestCaseTest.php
@@ -1,7 +1,5 @@
 <?php declare(strict_types=1);
 
-declare(strict_types=1);
-
 namespace RichCongress\Bundle\UnitBundle\Tests\TestCase;
 
 use Doctrine\Common\Annotations\Annotation\IgnoreAnnotation;
