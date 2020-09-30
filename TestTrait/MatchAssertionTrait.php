13d06224415248d4c81c16a5a5f655d5b9cf0458
Nicolas <novares.x@gmail.com>
2020-09-30 09:14:08 +0200

    Improve error message for assert match

--- TestTrait/MatchAssertionTrait.php
+++ TestTrait/MatchAssertionTrait.php
@@ -14,12 +14,14 @@ use RichCongress\Bundle\UnitBundle\TestTrait\Assertion\Parameter;
 trait MatchAssertionTrait
 {
     /**
-     * @param array $expected
-     * @param array $tested
+     * @param array  $expected
+     * @param array  $tested
+     *
+     * @param string $previousIndex
      *
      * @return void
      */
-    protected static function assertMatch(array $expected, array $tested): void
+    protected static function assertMatch(array $expected, array $tested, string $previousIndex = ''): void
     {
         // The array key presences are tested in the first place
         foreach (array_keys($expected) as $expectedKey) {
@@ -29,11 +31,12 @@ trait MatchAssertionTrait
         // The content of each key is then tested
         foreach ($expected as $expectedKey => $expectedMatch) {
             $testedValue = $tested[$expectedKey];
+            $totalIndex = $previousIndex !== null ? $previousIndex . '/' . $testedValue : (string) $testedValue;
 
             if (is_array($expectedMatch)) {
-                static::assertMatch($expectedMatch, $testedValue);
+                static::assertMatch($expectedMatch, $testedValue, $totalIndex);
             } else if ($expectedMatch instanceof Parameter) {
-                static::assertMatchParameter($expectedMatch, $testedValue);
+                static::assertMatchParameter($expectedMatch, $testedValue, $totalIndex);
             } else if ($expectedMatch !== null) {
                 static::assertEquals($expectedMatch, $testedValue);
             }
@@ -41,64 +44,78 @@ trait MatchAssertionTrait
     }
 
     /**
-     * @param Parameter $parameter
-     * @param mixed     $testedValue
+     * @param Parameter   $parameter
+     * @param mixed       $testedValue
+     * @param string|null $index
      *
      * @return void
      */
-    private static function assertMatchParameter(Parameter $parameter, $testedValue): void
+    private static function assertMatchParameter(Parameter $parameter, $testedValue, string $index = null): void
     {
-        static::assertTypeAndInstanceParameter($parameter, $testedValue);
+        static::assertTypeAndInstanceParameter($parameter, $testedValue, $index);
 
         // Regex test
         if ($parameter->regex !== null) {
-            self::assertRegExp($parameter->regex, $testedValue);
+            self::assertRegExp($parameter->regex, $testedValue, static::getError($index));
         }
 
         // Choice test
         if ($parameter->choice !== null) {
-            self::assertContains($testedValue, $parameter->choice);
+            self::assertContains($testedValue, $parameter->choice, static::getError($index));
         }
     }
 
     /**
-     * @param Parameter $parameter
-     * @param mixed     $testedValue
+     * @param Parameter   $parameter
+     * @param mixed       $testedValue
+     * @param string|null $index
      *
      * @return void
      */
-    private static function assertTypeAndInstanceParameter(Parameter $parameter, $testedValue): void
+    private static function assertTypeAndInstanceParameter(Parameter $parameter, $testedValue, string $index = null): void
     {
         if ($testedValue === null && $parameter->isNullable) {
-            self::assertNull($testedValue);
+            self::assertNull($testedValue, static::getError($index));
             return;
         }
 
         // Type test
         switch ($parameter->type) {
             case 'string':
-                self::assertIsString($testedValue);
+                self::assertIsString($testedValue, static::getError($index));
                 return;
 
             case 'integer':
-                self::assertIsInt($testedValue);
+                self::assertIsInt($testedValue, static::getError($index));
                 return;
 
             case 'float':
-                self::assertIsFloat($testedValue);
+                self::assertIsFloat($testedValue, static::getError($index));
                 return;
 
             case 'array':
-                self::assertIsArray($testedValue);
+                self::assertIsArray($testedValue, static::getError($index));
                 return;
 
             case 'boolean':
-                self::assertIsBool($testedValue);
+                self::assertIsBool($testedValue, static::getError($index));
                 return;
         }
 
         if ($parameter->class !== null) {
-            self::assertInstanceOf($parameter->class, $testedValue);
+            self::assertInstanceOf($parameter->class, $testedValue, static::getError($index));
         }
+    }
+
+    /**
+     * @param string|null $index
+     *
+     * @return string
+     */
+    private static function getError(string $index = null): string
+    {
+        return $index !== null
+            ? sprintf('Error during the match assertion for the index "%s".', $index)
+            : 'Error during the match assertion.';
     }
 }
